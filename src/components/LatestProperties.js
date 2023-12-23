83b192995194e846f21e89dac67d54030f6d4292
William Baele <baelewilliam1999@gmail.com>
2023-12-23 15:37:43 +0100

    added some stuff

--- src/components/LatestProperties.js
+++ src/components/LatestProperties.js
@@ -1,23 +1,60 @@
 import React from "react";
 import PropertyCard from "./PropertyCard";
+import { GoArrowUpRight } from "react-icons/go";
+
+// Import Swiper styles
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
 
 const LatestProperties = () => {
   return (
-    <div className="bg-red-200 w-full py-10 md:py-16">
+    <div className="bg-[#FBFBFE] w-full py-10 md:py-16">
       <div className="container mx-auto px-4 md:px-0">
-        <div className="flex justify-between bg-green-400">
+        <div className="flex justify-between ">
           <div className="flex flex-col gap-2 md:gap-4">
             <h2 className="text-xl md:text-2xl font-bold">
               Explore our latest properties
             </h2>
             <p>Find your gem from 1800+ properties</p>
           </div>
-          <div className="bg-yellow-600 flex items-end">
-            <p>hi</p>
+          <div className=" flex items-end justify-between gap-2 cursor-pointer">
+            <p>See all properties</p>
+            <GoArrowUpRight style={{ fontSize: "20px" }} />
           </div>
         </div>
-        <div className="w-1/3 bg-blue-400 py-10 px-4">
-          <PropertyCard />
+        <div className="w-full gap-4 py-10 px-4">
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            freeMode={true}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[FreeMode, Pagination]}
+            className=" w-full"
+          >
+            <SwiperSlide>
+              <PropertyCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              <PropertyCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              <PropertyCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              <PropertyCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              <PropertyCard />
+            </SwiperSlide>
+            <SwiperSlide>
+              <PropertyCard />
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </div>
--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,5 +1,8 @@
 import React from "react";
 import HomeImage from "../assets/images/home1.jpeg";
+import { IoBedOutline } from "react-icons/io5";
+import { LuBath } from "react-icons/lu";
+import { SlSizeActual } from "react-icons/sl";
 
 const PropertyCard = () => {
   const cardStyle = {
@@ -8,19 +11,47 @@ const PropertyCard = () => {
     backgroundPosition: "center",
   };
 
+  const options = [
+    {
+      name: "Bedrooms",
+      svg: <IoBedOutline />,
+      info: "4",
+    },
+    {
+      name: "Bathrooms",
+      svg: <LuBath />,
+      info: "2",
+    },
+    {
+      name: "Surface",
+      svg: <SlSizeActual />,
+      info: "200",
+    },
+  ];
   return (
-    <div className="bg-white shadow-lg h-80 w-full rounded-lg">
+    <div className="bg-white shadow-lg h-80 w-full rounded-lg cursor-pointer">
       <div
         className="h-3/5 bg-green-600 rounded-t-lg relative w-full"
         style={cardStyle}
       >
-        <p className="text-sm absolute left-2 bottom-2 bg-white shadow-md rounded-md px-2">
+        <p className="text-sm absolute left-3 bottom-3 bg-white shadow-md rounded-md px-2">
           300,000€
         </p>
       </div>
-      <div className="h-2/5 w-full py-4 px-2 flex flex-col gap-2">
+      <div className="h-2/5 w-full py-4 px-4 flex flex-col gap-2">
         <h3 className="font-bold">Luxury villa in Etterbeek</h3>
         <p className="text-gray-400">Etterbeek 1050, Belgium</p>
+        <div className="w-full grid grid-cols-3 gap-2">
+          {options.map((option, index) => (
+            <div
+              key={index}
+              className="hover:bg-gray-100 flex items-center gap-2 rounded-md px-1"
+            >
+              {option.svg}
+              <p>{option.info}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
